7857408450075ec67041becea7f6c72ece891be9
ELJUEEN <ELJUEEN@192.168.1.23>
2021-06-09 20:15:33 -0300

    x2

--- TP7-Patrones-ej2/src/Main.java
+++ TP7-Patrones-ej2/src/Main.java
@@ -1,23 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
-		DBFacade facadeahre = new FacadeImplement();
+		DBFacade facade = new FacadeImplement();
 
 		String sql = "select * from personas p left join telefonos t on (p.id = t.idpersona)";
 
-		List<String[]> ahre = facadeahre.queryResultAsArray(sql);
+		List<String[]> lista = facade.queryResultAsArray(sql);
 
-		for (String[] strings : ahre) {
+		for (String[] strings : lista) {
 			System.out.println(Arrays.deepToString(strings));
 		}
 
-		// List<Map<String, String>> listaValores =
-		// facadeahre.queryResultAsAsociation(sql);
-
-		// System.out.println(listaValores);
+		List<Map<String, String>> listaValores = facade.queryResultAsAsociation(sql);
+		System.out.println(listaValores);
 
 	}
 
